1a2fc3f0ff064f4ce18f96de95db816cced9c8f3
Gauravd1710 <gauravdongre2004@gmail.com>
2026-06-27 00:25:20 +0530

    feat: implemented finance accounts and transactions

--- backend/src/modules/finance/finance.controller.ts
+++ backend/src/modules/finance/finance.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateAccountDto } from './dto/crreate-account.dto';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { FinanceService } from './finance.service';
 
 @Controller('finance')
-export class FinanceController {}
+export class FinanceController {
+  constructor(private readonly financeService: FinanceService) {}
+
+  @Post('accounts')
+  createAccount(@Body() dto: CreateAccountDto) {
+    return this.financeService.createAccount(dto);
+  }
+
+  @Get('accounts')
+  getAccounts() {
+    return this.financeService.getAccounts();
+  }
+
+  @Post('transactions')
+  createTransaction(@Body() dto: CreateTransactionDto) {
+    return this.financeService.createTransaction(dto);
+  }
+
+  @Get('transactions')
+  getTransactions() {
+    return this.financeService.getTransactions();
+  }
+}
